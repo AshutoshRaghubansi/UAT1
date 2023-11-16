7c01fa70ca9c5debc6811e6304f330323d65aada
Ashutosh <ashutosh.raghubansi@pramericalife.in>
2023-11-16 12:37:34 +0530

    server.js2

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,3 @@
-const path = require('path');
-
 module.exports = {
-  entry: '/src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
-  },
-  resolve: {
-    modules: [
-      'node_modules', // The default
-      'src'
-    ]
-  }
-}
+  target: 'node',
+};
